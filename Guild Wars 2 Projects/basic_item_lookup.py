3469887caeb9765dce795061a4a082480a232ef9
Adam Imane <adamdeardon@hotmail.com>
2026-06-11 06:46:43 +0100

    Established greater modularity within the code and update QoL:
    - Moved actual lookup into its own reusable method.
    - Moved input validation into its own method.
    - Created a main method for the entry point for easier maintenance later.
    
    - Program now allows for multiple inputs (does not exit unless users wants to).
    - Clears the screen on entry (mostly for early development testing).
    - Now displays the error code returned on unnsuccessful lookup (404 for unfound items, etc).
    
    - Also added some TODOs in TODO.txt so I remember what I want to do as development progresses.

--- Guild Wars 2 Projects/basic_item_lookup.py
+++ Guild Wars 2 Projects/basic_item_lookup.py
@@ -1,24 +1,52 @@
 import requests
+import os
 
-# TODO: Get price information (Trading post).
-# TODO: Allow for names to be entered to get information.
-# TODO: Allow for repeated attempts after an invalid input.
+# Function that takes a valid input and checks if the item exists.
+# Returns the item name for the ID or a status code if invalid.
+def lookup_by_Id(input):
 
-# Input
-item = input("Enter the ID of the item you are looking for: ")
-
-# Validate
-if not item.isdigit():
-    print("Please enter a numeric item ID.")
-
-else:
     # Using requests.get to get the input item's information using ID
-    response = requests.get(f"https://api.guildwars2.com/v2/items/{item}")
+    response = requests.get(f"https://api.guildwars2.com/v2/items/{input}")
     data = response.json()
 
     if response.status_code == 200:
         print(data["name"])
-    
+
     else:
-        print("Could not find that item.")
+        print("Error ", response.status_code, ": Could not find that item.", )
         print("API message:", data.get("text", "Unknown error"))
+
+# Function that validates that the input is a valid ID (digit)
+def validate_input(item):
+    if not item.isdigit():
+        print("Please enter a numeric item ID.")
+
+    else:
+        return True
+
+# Program entry point
+def main():
+    
+    # Repeating loop
+    searching = True
+    while searching == True:
+
+        # Check input is valid first
+        valid = False
+        while valid == False:
+            item = input("Enter the ID of the item you are looking for: ")
+            if validate_input(item) == True:
+                valid = True
+    
+        # Lookup item
+        lookup_by_Id(item)
+
+        search_check = input("\nPress x to quit, or any other key to continue: ")
+        if (search_check == "x"):
+            print("Thank you for using the lookup service! Goodbye!")
+            searching = False
+
+# Clearing the console on Entry TODO: Remove this when I move to frontend development.
+clear = lambda: os.system('cls')
+clear()
+main()
