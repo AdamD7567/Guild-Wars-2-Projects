ec7ba7a38e4167ce91d8b4593016e4aa5668a7f0
Adam Imane <adamdeardon@hotmail.com>
2026-06-11 07:12:04 +0100

    Added functionality that looks-up the item's current buy and sell price.
    - Takes the validated input, parses the JSON file, and returns the unit_price for both buy and sell as a single set of data.
    
    - Added a helper function that converts the returned raw copper value and formats it as gold, copper, and silver.

--- Guild Wars 2 Projects/basic_item_lookup.py
+++ Guild Wars 2 Projects/basic_item_lookup.py
@@ -1,6 +1,14 @@
 import requests
 import os
 
+# Function that validates that the input is a valid ID (digit)
+def validate_input(item):
+    if not item.isdigit():
+        print("Please enter a numeric item ID.")
+
+    else:
+        return True
+
 # Function that takes a valid input and checks if the item exists.
 # Returns the item name for the ID or a status code if invalid.
 def lookup_by_Id(input):
@@ -11,20 +19,44 @@ def lookup_by_Id(input):
 
     if response.status_code == 200:
         print(data["name"])
+        price_info = lookup_prices(input)
+
+        if price_info is not None:
+            print(price_info)
 
     else:
         print("Error ", response.status_code, ": Could not find that item.", )
         print("API message:", data.get("text", "Unknown error"))
 
-# Function that validates that the input is a valid ID (digit)
-def validate_input(item):
-    if not item.isdigit():
-        print("Please enter a numeric item ID.")
+# Function that looks up the items buy and sell price on the Trading post
+# Takes in the item's ID and returns the current buy and sell price
+def lookup_prices(ID):
+
+    response = requests.get(f"https://api.guildwars2.com/v2/commerce/prices/{ID}")
+    data = response.json()
+    
+    if response.status_code == 200:
+        buy_price = data["buys"]["unit_price"]
+        sell_price = data["sells"]["unit_price"]
+        return f"Buy price: {format_coins(buy_price)}\nSell price: {format_coins(sell_price)}"
 
     else:
-        return True
+        print(
+            "Error ", 
+            response.status_code, 
+            ": Could not find Trading Post price.\nThis item may not be on the trading post."
+            )
 
-# Program entry point
+# Helper function that converts raw copper value into a readable format
+# Takes in the copper value and returns the gold, silver, and copper value
+def format_coins(copper):
+    gold = copper // 10000
+    silver = (copper % 10000) // 100
+    copper = copper % 100
+
+    return f"{gold}g {silver}s {copper}c"
+
+# Program's entry point
 def main():
     
     # Repeating loop
